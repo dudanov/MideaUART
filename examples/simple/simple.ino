52cc8b36903f30ca0b7b79b69ec54b782acb0eb1
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2021-08-23 20:58:33 +0400

    Add simple example

---
+++ examples/simple/simple.ino
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#include <Appliance/AirConditioner/AirConditioner.h>
+
+dudanov::midea::ac::AirConditioner ac;
+
+// Example how can change work mode easily
+static inline void switchMode(dudanov::midea::Mode mode) {
+  dudanov::midea::ac::Control control;
+  control.mode = mode;
+  ac.control(control);
+}
+
+// Example how can change mode and temp in same time
+static inline void changeState(dudanov::midea::Mode mode, float targetTemp) {
+  dudanov::midea::ac::Control control;
+  control.mode = mode;
+  control.targetTemp = targetTemp;
+  ac.control(control);
+}
+
+// Example how can change mode to AUTO and set target temp to 25C
+static inline void changeAuto25() {
+  dudanov::midea::ac::Control control;
+  control.mode = dudanov::midea::Mode::MODE_AUTO;
+  control.targetTemp = 25.0f;
+  ac.control(control);
+}
+
+// Here you may get new properties states
+void onStateChange() {
+  ac.getTargetTemp();
+  ac.getIndoorTemp();
+  ac.getMode();
+  ac.getPreset();
+  ac.getSwingMode();
+  ac.getFanMode();
+}
+
+void setup() {
+  Serial.begin(9600);     // set serial baudrate to 9600 8N1
+  ac.setStream(&Serial);  // set stream serial interface
+  ac.addOnStateCallback(onStateChange); // add callback
+  ac.setup();
+}
+
+void loop() {
+  ac.loop();
+}

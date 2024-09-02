2f1b3fccbe1b0defde45c9e5da405b2ba4a4357e
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2024-09-02 13:48:42 +0400

    format

--- src/Appliance/ApplianceBase.cpp
+++ src/Appliance/ApplianceBase.cpp
@@ -16,8 +16,10 @@ static const char *TAG = "ApplianceBase";
 ResponseStatus ApplianceBase::Request::callHandler(const Frame &frame) {
   if (!frame.hasType(this->requestType))
     return ResponseStatus::RESPONSE_WRONG;
+
   if (this->onData == nullptr)
     return RESPONSE_OK;
+
   return this->onData(frame.getData());
 }
 
@@ -26,6 +28,7 @@ static bool read_frame(Frame &frame, Stream *stream) {
     if (frame.deserialize(stream->read()))
       return true;
   }
+
   return false;
 }
 
@@ -33,10 +36,12 @@ void ApplianceBase::setup() {
   m_timerManager.registerTimer(m_periodTimer);
   m_timerManager.registerTimer(m_networkTimer);
   m_timerManager.registerTimer(m_responseTimer);
+
   m_networkTimer.setCallback([this](Timer *timer) {
     m_sendNetworkNotify();
     timer->reset();
   });
+
   m_networkTimer.start(2 * 60 * 1000);
   m_networkTimer.call();
   m_setup();
@@ -69,7 +74,9 @@ void ApplianceBase::loop() {
 
   m_request = m_queue.front();
   m_queue.pop_front();
+
   LOG_D(TAG, "Getting and sending a request from the queue...");
+
   m_sendRequest(m_request);
 
   if (m_request->onData != nullptr) {
@@ -84,37 +91,48 @@ void ApplianceBase::loop() {
 void ApplianceBase::m_handler(const Frame &frame) {
   if (m_isWaitForResponse()) {
     auto result = m_request->callHandler(frame);
+
     if (result != RESPONSE_WRONG) {
       if (result == RESPONSE_OK) {
         if (m_request->onSuccess != nullptr)
           m_request->onSuccess();
+
         m_destroyRequest();
+
       } else {
         m_resetAttempts();
         m_resetTimeout();
       }
+
       return;
     }
   }
+
   // ignoring responses on network notifies
   if (frame.hasType(NETWORK_NOTIFY))
     return;
+
   /* HANDLE REQUESTS */
   if (frame.hasType(QUERY_NETWORK)) {
     m_sendNetworkNotify(QUERY_NETWORK);
     return;
   }
+
   m_onRequest(frame);
 }
 
 static uint8_t getSignalStrength() {
   const int32_t dbm = WiFi.RSSI();
+
   if (dbm > -63)
     return 4;
+
   if (dbm > -75)
     return 3;
+
   if (dbm > -88)
     return 2;
+
   return 1;
 }
 
@@ -124,9 +142,11 @@ void ApplianceBase::m_sendNetworkNotify(FrameType msgType) {
   notify.setSignalStrength(getSignalStrength());
   notify.setIP(WiFi.localIP());
   notify.appendCRC();
+
   if (msgType == NETWORK_NOTIFY) {
     LOG_D(TAG, "Enqueuing a DEVICE_NETWORK(0x0D) notification...");
     m_queueNotify(msgType, std::move(notify));
+
   } else {
     LOG_D(TAG, "Answer to QUERY_NETWORK(0x63) request...");
     m_sendFrame(msgType, std::move(notify));
@@ -136,21 +156,27 @@ void ApplianceBase::m_sendNetworkNotify(FrameType msgType) {
 void ApplianceBase::m_resetTimeout() {
   m_responseTimer.setCallback([this](Timer *timer) {
     LOG_D(TAG, "Response timeout...");
+
     if (!--m_remainAttempts) {
       if (m_request->onError != nullptr)
         m_request->onError();
+
       m_destroyRequest();
       return;
     }
+
     LOG_D(TAG, "Sending request again. Attempts left: %d...", m_remainAttempts);
+
     m_sendRequest(m_request);
     m_resetTimeout();
   });
+
   m_responseTimer.start(m_timeout);
 }
 
 void ApplianceBase::m_destroyRequest() {
   LOG_D(TAG, "Destroying the request...");
+
   m_responseTimer.stop();
   delete m_request;
   m_request = nullptr;
@@ -158,30 +184,36 @@ void ApplianceBase::m_destroyRequest() {
 
 void ApplianceBase::m_sendFrame(FrameType type, const FrameData &data) {
   auto frame = Frame::make(m_applianceID, m_protocolID, type, data);
+
   LOG_D(TAG, "TX: %s", frame.toString().c_str());
+
   m_stream->write(frame.data(), frame.size());
   m_isBusy = true;
   m_periodTimer.setCallback([this](Timer *timer) {
     m_isBusy = false;
     timer->stop();
   });
+
   m_periodTimer.start(m_period);
 }
 
 void ApplianceBase::m_queueRequest(FrameType type, FrameData &&data, ResponseHandler onData, Handler onSucess,
                                    Handler onError) {
   LOG_D(TAG, "Enqueuing the request...");
+
   m_queue.push_back(new Request{std::move(data), std::move(onData), std::move(onSucess), std::move(onError), type});
 }
 
 void ApplianceBase::m_queueRequestPriority(FrameType type, FrameData &&data, ResponseHandler onData, Handler onSucess,
                                            Handler onError) {
   LOG_D(TAG, "Priority request queuing...");
+
   m_queue.push_front(new Request{std::move(data), std::move(onData), std::move(onSucess), std::move(onError), type});
 }
 
 void ApplianceBase::setBeeper(bool value) {
   LOG_D(TAG, "Turning %s beeper feedback...", value ? "ON" : "OFF");
+
   m_beeper = value;
 }
 
